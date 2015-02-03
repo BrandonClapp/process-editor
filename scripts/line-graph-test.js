e3c98f44befa9d6a103a95e32d65455e50b12f62
Brandon Clapp <clapp09@gmail.com>
2015-02-02 21:21:29 -0600

    d3.scale examples

--- scripts/line-graph-test.js
+++ scripts/line-graph-test.js
@@ -1,7 +1,7 @@
 <!--Line Graph-->
 
-var h = 350;
-var w = 400;
+var h = window.screen.height;
+var w = window.screen.width;
 
 monthlySales = [
 {"month": 10, "sales": 100},
@@ -16,51 +16,53 @@ monthlySales = [
 {"month": 100, "sales": 130}
 ];
 
+// https://github.com/mbostock/d3/wiki/Quantitative-Scales#linear-scales
+var xScale = d3.scale.linear()
+              .domain([
+                d3.min(monthlySales, function(d){ return d.month; }),
+                d3.max(monthlySales, function(d){ return d.month; })
+                ])
+              .range([0, w]);
+
+var yScale = d3.scale.linear()
+              .domain([0, d3.max(monthlySales, function(d){ return d.sales })])
+              .range([h, 0]); // inversed because of svg coordinates being reversed.
+
 var lineFun = d3.svg.line()
-  .x(function(d) {
-    return d.month * 4;
-  })
-  .y(function(d) {
-    return h - d.sales;
-  })
-  .interpolate('linear');
+              .x(function(d) { return xScale(d.month); })
+              .y(function(d) { return yScale(d.sales); })
+              .interpolate('linear');
 
 var svg = d3.select("div#testing").append("svg").attr({
-  width: w,
-  height: h
-});
+            width: w,
+            height: h
+          });
 
 var viz = svg.append("path")
-  .attr({
-    d: lineFun(monthlySales),
-    "stroke": "purple",
-    "stroke-width": 2,
-    "fill": "none"
-  });
+              .attr({
+                d: lineFun(monthlySales),
+                "stroke": "purple",
+                "stroke-width": 2,
+                "fill": "none"
+              });
 
 // add labels
 var labels = svg.selectAll("text").data(monthlySales).enter()
-  .append("text")
-  .text(function(d) {
-    return d.sales;
-  })
-  .attr({
-    x: function(d) {
-      return (d.month * 4) - 25;
-    },
-    y: function(d) {
-      return h - d.sales;
-    },
-    "font-family": "sans-serif",
-    "font-size": "12px",
-    "fill": "#666666",
-    "text-anchor": "start",
-    "dy": ".35em", // line size
-    "font-weight": function(d, i) {
-      if (i === 0 || i === monthlySales.length - 1) { // if is first element or last element in array
-        return "bold";
-      } else {
-        return "normal";
-      }
-    }
-  });
+              .append("text")
+              .text(function(d) { return d.sales; })
+                .attr({
+                  x: function(d) { return xScale(d.month) - 25; },
+                  y: function(d) { return yScale(d.sales); },
+                  "font-family": "sans-serif",
+                  "font-size": "12px",
+                  "fill": "#666666",
+                  "text-anchor": "start",
+                  "dy": ".35em", // line size
+                  "font-weight": function(d, i) {
+                    if (i === 0 || i === monthlySales.length - 1) { // if is first element or last element in array
+                      return "bold";
+                    } else {
+                      return "normal";
+                    }
+                  }
+                });
